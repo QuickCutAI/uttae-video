614577d6bae31c189726e3a3eba39e2a6a131063
saebom <saebom96@naver.com>
2023-11-14 20:21:15 +0900

    Layout

--- trevari/src/App.tsx
+++ trevari/src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { css } from "@emotion/css";
+import { Layout } from "antd";
+import { Content, Header } from "antd/es/layout/layout";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <Layout>
+        <Header
+          className={css`
+            color: #fff;
+            text-align: center;
+          `}
         >
-          Learn React
-        </a>
-      </header>
+          ItBook
+        </Header>
+        <Content
+          className={css`
+            padding: 0 24px;
+          `}
+        >
+          <Routes>
+            <Route path="/books" />
+            <Route path="/books/:id" />
+          </Routes>
+        </Content>
+      </Layout>
     </div>
   );
 }
